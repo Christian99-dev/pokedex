a3d9cbfdae15a0fd2f2c868ab437904d087e0341
Christian Keller <k.christian9@web.de>
2023-12-01 14:17:20 +0100

    feat: button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,29 +1,60 @@
 import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
+import { responsiveCSS } from "@/theme/responsive";
 
 
 const Button = ({ text, route }: { text: string; route: string }) => {
   return (
-    <Link href={route} passHref>
-      <ButtonWrapper>{text}</ButtonWrapper>
-    </Link>
+    <LinkWrapper href={route} passHref>
+      <div className="fill"></div>
+      <button>{text}</button>
+    </LinkWrapper>
   );
 };
 
 export default Button;
 
-const ButtonWrapper = styled.button`
-background-color: darkblue;
-padding: 10px;
-color: white;
-border: none;
-font-size: var(--fs-3);
-margin: 0 var(--space-md); /* Added margin between buttons */
-border-radius: 8px; /* Added rounded corners */
-cursor: pointer; /* Add pointer cursor for better UX */
+const LinkWrapper = styled(Link)`
+  position: relative;
+  button{
+    position: relative;
+    z-index: 2;
+    background-color: transparent;
+    border: 3px solid var(--pink);
+    padding: 10px 0;
+    color: white;
+    font-size: var(--fs-3);
+    color: var(--pink);
+    font-weight: 600;
+    border-radius: 8px;
+    ${responsiveCSS("width", 260,240,220,200,190,180)}
+    cursor: pointer; 
+    transition: all 0.3s ease;
+  } 
 
-&:hover {
-  background-color: #001f3f; /* Change color on hover */
-}
-`;
+  .fill{
+    position: absolute;
+    z-index:1;
+    width: 0%;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    background-color: var(--pink);
+    border-radius: 8px;
+    transition: all 0.3s ease;
+  }
+
+  &:hover {
+    .fill{
+      width: 100%;
+      transition: all 0.3s ease;
+    }
+    
+    button{
+      color: var(--dark);
+      transition: all 0.3s ease;
+    }
+  }
+`
+
