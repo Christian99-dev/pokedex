51d5ed81d4dd5032b79b07ca6dad04e50d9f50ab
Christian Keller <k.christian9@web.de>
2024-02-03 20:31:25 +0100

    fix: clean up context code

--- src/context/PokemonContext.tsx
+++ src/context/PokemonContext.tsx
@@ -40,7 +40,7 @@ export const PokemonProvider = ({
   const [pokemonData, setPokemonData] = useState<Pokemon[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [allTypes, setAllTypes] = useState<string[]>([]);
-  
+
   const getPokemonById = (id: number) =>
     isLoading ? null : pokemonData.find((pokemon) => pokemon.id === id);
 
@@ -51,38 +51,36 @@ export const PokemonProvider = ({
     const last = isLoading ? 0 : pokemonData[pokemonData.length - 1].id || 0;
     return { first, last };
   };
-  
 
   useEffect(() => {
-    const fetchPokemons = async () => {
-      try {
-        const { data } = await client.query({
-          query: gql`
-            query MyQuery {
-              pokemon_v2_pokemon(limit: 1009) {
-                id
-                name
-                pokemon_v2_pokemontypes {
-                  pokemon_v2_type {
-                    name
-                  }
-                }
-                pokemon_v2_pokemonsprites {
-                  sprites
+    client
+      .query({
+        query: gql`
+          query MyQuery {
+            pokemon_v2_pokemon(limit: 493) {
+              id
+              name
+              pokemon_v2_pokemontypes {
+                pokemon_v2_type {
+                  name
                 }
               }
+              pokemon_v2_pokemonsprites {
+                sprites
+              }
             }
-          `,
-        });
+          }
+        `,
+      })
+      .then(({data}) => {
         let allTypesWithDuplicate: string[] = [];
 
+        // Adding session pokemons
+        const sessionPokemons: Pokemon[] = getAllPokemonsFromSession();
 
-        if (data && data.pokemon_v2_pokemon) {
-          // Adding session pokemons
-          const sessionPokemons: Pokemon[] = getAllPokemonsFromSession();
-
-          // Parsing pokemons from graphql
-          const parsedPokemons: Pokemon[] = data.pokemon_v2_pokemon.map((pokemon: any) => {
+        // Parsing pokemons from graphql
+        const parsedPokemons: Pokemon[] = data.pokemon_v2_pokemon.map(
+          (pokemon: any) => {
             let allTypes = pokemon.pokemon_v2_pokemontypes.map(
               (type: any) => type.pokemon_v2_type.name
             );
@@ -97,21 +95,19 @@ export const PokemonProvider = ({
               description:
                 "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidu",
             };
-          });
+          }
+        );
 
-          setPokemonData([...sessionPokemons, ...parsedPokemons]);
-          setAllTypes(Array.from(new Set(allTypesWithDuplicate)));
-        }
-      } catch (error) {
+        setPokemonData([...sessionPokemons, ...parsedPokemons]);
+        setAllTypes(Array.from(new Set(allTypesWithDuplicate)));
+      })
+      .catch((error) => {
         console.error("Error fetching Pokemon data:", error);
-      } finally {
+      })
+      .finally(() => {
         setIsLoading(false);
-      }
-    };
-
-    fetchPokemons();
+      });
   }, []);
-
 
   return (
     <PokemonContext.Provider
@@ -127,4 +123,3 @@ export const PokemonProvider = ({
     </PokemonContext.Provider>
   );
 };
-
